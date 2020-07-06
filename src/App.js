a52d2a9b76ec1761d4c06eb3a107cb4ed70bdf66
Indiana Kuffer <indianakuffer@gmail.com>
2020-07-06 12:48:57 -0400

    hide api key

--- src/App.js
+++ src/App.js
@@ -57,8 +57,8 @@ export default function App() {
   }, [])
 
   const getIssEarthLocation = async (lon, lat) => {
-    // const apiKey = process.env.REACT_APP_OCG_API_KEY
-    const apiKey = '777ea9e72f594073a460308bbbe9d2ca'
+    const apiKey = process.env.REACT_APP_OCG_API_KEY
+    // const apiKey = '777ea9e72f594073a460308bbbe9d2ca'
     const q = `${lat},${lon}`
     try {
       const response = await axios.get(`https://api.opencagedata.com/geocode/v1/json?key=${apiKey}&q=${q}`)
